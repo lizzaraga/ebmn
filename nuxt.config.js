cda2571587e9c65cb0a0480ec22c48171c717963
Christ Mael <fomekongchristmael@gmail.com>
2021-04-06 20:59:16 +0100

    Add icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
             { hid: 'description', name: 'description', content: '' }
         ],
         link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }, { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" }
         ]
     },
 
